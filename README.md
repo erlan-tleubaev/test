80480e5c19b814a1a12022034d0babf51bd5d810
erlan <teamosh25@gmail.com>
2025-10-17 11:07:43 +0500

    добавление некоторых функции

---
+++ README.md
@@ -0,0 +1,15 @@
+Usage:
+
+изначальная версия для Aitpa Messenger (AMMAG TECHNOLOGIES LLP)
+
+Слушатель: 
+```
+ncat -k -l 0.0.0.0 7777 | encryptor.exe supersecret123
+```
+
+Отправитель:
+```
+echo "Password is Passw0rd1884!@#" | encryptor.exe supersecret123 | ncat 
+localhost 7777
+```
+![usage](image-1.png)
--- enc.c
+++ enc.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int encrypt(int n){
-  return c ^ 17;
+
+unsigned long hash(const char *str){
+  unsigned long hash = 5381;
+  int c;
+
+  while(c = *str++)
+          hash = ((hash << 5)+hash) + c;
+  
+  
+  return hash;
+}
+
+int encrypt(int n, const char *key){
+  int key_hash = hash(key);
+  return n ^ key_hash;
 }
 
 
@@ -12,8 +25,10 @@ int main(int argc, const char *argv[]){
       return -1;
   }
   int c;
+  const char *input_key = argv[1];
+  
   while ((c = getc(stdin)) != EOF){
-    int c_enc = encrypt(c);
+    int c_enc = encrypt(c, input_key);
     putc(c_enc,stdout);
 }
 
